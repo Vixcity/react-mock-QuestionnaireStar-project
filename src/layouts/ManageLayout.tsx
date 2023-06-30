f14a18defe00c0b2431932d52ffdbefae9240df5
Vixcity <2091283625@qq.com>
2023-06-30 09:48:53 +0800

    使用 AntD 重构 ManageLayout - 灵活使用 Button

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,19 +1,53 @@
 import React, { FC } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import {
+  PlusOutlined,
+  BarsOutlined,
+  StarOutlined,
+  DeleteOutlined,
+} from "@ant-design/icons";
+import { Button, Space, Divider } from "antd";
 import styles from "./ManageLayout.module.scss";
 
 const ManageLayout: FC = () => {
+  const nav = useNavigate();
+  const { pathname } = useLocation();
+  console.log("pathname", pathname);
+
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <p>ManageLayout header</p>
-        <button>创建问卷</button>
-        <br />
-        <a href="#">我的问卷</a>
-        <br />
-        <a href="#">星标问卷</a>
-        <br />
-        <a href="#">回收站</a>
+        <Space direction="vertical">
+          <Button type="primary" size="large" icon={<PlusOutlined />}>
+            新建问卷
+          </Button>
+          <Divider style={{ borderTop: "transparent" }} />
+          <Button
+            type={pathname.startsWith("/manage/list") ? "default" : "text"}
+            onClick={() => nav("/manage/list")}
+            size="large"
+            icon={<BarsOutlined />}
+          >
+            我的问卷
+          </Button>
+          <Button
+            type={pathname.startsWith("/manage/star") ? "default" : "text"}
+            onClick={() => nav("/manage/star")}
+            size="large"
+            icon={<StarOutlined />}
+          >
+            星标问卷
+          </Button>
+          <Button
+            type={pathname.startsWith("/manage/trash") ? "default" : "text"}
+            onClick={() => nav("/manage/trash")}
+            size="large"
+            icon={<DeleteOutlined />}
+          >
+            回收站
+          </Button>
+        </Space>
       </div>
       <div className={styles.right}>
         <Outlet />
