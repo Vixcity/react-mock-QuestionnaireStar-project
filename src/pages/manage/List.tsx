f3cd068faeb1b6615e63661f721018e85340f850
Vixcity <2091283625@qq.com>
2023-06-30 11:42:19 +0800

    使用 AntD 开发星标问卷列表页

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,55 +1,58 @@
 import React, { FC, useState } from "react";
 import styles from "./common.module.scss";
+import { Typography } from "antd";
 import QuestionCard from "../../components/QuestionCard";
 import { useTitle } from "ahooks";
 // import { useSearchParams } from "react-router-dom";
+
+const { Title } = Typography;
+
+const rawQuestionList = [
+  {
+    _id: "q1", // mongodb 数据库 中的 id 都是带下划线的，为了和数据库统一，所以采用 _id
+    title: "问卷1",
+    isPublished: false,
+    isStar: false,
+    answerCount: 5,
+    createdAt: "3月10日 13:23",
+  },
+  {
+    _id: "q2",
+    title: "问卷2",
+    isPublished: true,
+    isStar: false,
+    answerCount: 3,
+    createdAt: "3月11日 13:23",
+  },
+  {
+    _id: "q3",
+    title: "问卷3",
+    isPublished: false,
+    isStar: true,
+    answerCount: 6,
+    createdAt: "3月12日 13:23",
+  },
+  {
+    _id: "q4",
+    title: "问卷4",
+    isPublished: true,
+    isStar: false,
+    answerCount: 2,
+    createdAt: "3月15日 13:23",
+  },
+];
 
 const List: FC = () => {
   useTitle("小温问卷 - 我的问卷");
   // const [searchParams] = useSearchParams();
   // console.log("keyword:", searchParams.get("keyword"));
 
-  const rawQuestionList = [
-    {
-      _id: "q1", // mongodb 数据库 中的 id 都是带下划线的，为了和数据库统一，所以采用 _id
-      title: "问卷1",
-      isPublished: false,
-      isStar: false,
-      answerCount: 5,
-      createdAt: "3月10日 13:23",
-    },
-    {
-      _id: "q2",
-      title: "问卷2",
-      isPublished: true,
-      isStar: false,
-      answerCount: 3,
-      createdAt: "3月11日 13:23",
-    },
-    {
-      _id: "q3",
-      title: "问卷3",
-      isPublished: false,
-      isStar: true,
-      answerCount: 6,
-      createdAt: "3月12日 13:23",
-    },
-    {
-      _id: "q4",
-      title: "问卷4",
-      isPublished: true,
-      isStar: false,
-      answerCount: 2,
-      createdAt: "3月15日 13:23",
-    },
-  ];
-
   const [questionList, setQuestionList] = useState(rawQuestionList);
   return (
     <>
       <div className={styles.header}>
         <div className={styles.left}>
-          <h3>我的问卷</h3>
+          <Title level={3}>我的问卷</Title>
         </div>
         <div className={styles.right}>搜索</div>
       </div>
--- src/pages/manage/Star.tsx
+++ src/pages/manage/Star.tsx
@@ -1,7 +1,63 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import styles from "./common.module.scss";
+import { Typography, Empty } from "antd";
+import QuestionCard from "../../components/QuestionCard";
+import { useTitle } from "ahooks";
+// import { useSearchParams } from "react-router-dom";
+
+const { Title } = Typography;
+
+const rawQuestionList = [
+  {
+    _id: "q1", // mongodb 数据库 中的 id 都是带下划线的，为了和数据库统一，所以采用 _id
+    title: "问卷1",
+    isPublished: false,
+    isStar: true,
+    answerCount: 5,
+    createdAt: "3月10日 13:23",
+  },
+  {
+    _id: "q2",
+    title: "问卷2",
+    isPublished: true,
+    isStar: true,
+    answerCount: 3,
+    createdAt: "3月11日 13:23",
+  },
+  {
+    _id: "q3",
+    title: "问卷3",
+    isPublished: false,
+    isStar: true,
+    answerCount: 6,
+    createdAt: "3月12日 13:23",
+  },
+];
 
 const Star: FC = () => {
-  return <p>Star</p>;
+  useTitle("小温问卷 - 星标问卷");
+
+  const [questionList, setQuestionList] = useState(rawQuestionList);
+
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <Title level={3}>星标问卷</Title>
+        </div>
+        <div className={styles.right}>搜索</div>
+      </div>
+      <div className={styles.content}>
+        {questionList.length === 0 && <Empty description="暂无数据" />}
+        {questionList.length > 0 &&
+          questionList.map((q) => {
+            const { _id } = q;
+            return <QuestionCard key={_id} {...q} />;
+          })}
+      </div>
+      <div className={styles.footer}>分页</div>
+    </>
+  );
 };
 
 export default Star;
