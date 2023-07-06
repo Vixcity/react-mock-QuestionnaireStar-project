550547e1915e84e62a6c9a20f3c38811039d716d
Vixcity <2091283625@qq.com>
2023-07-06 09:27:56 +0800

    antd 表单组件校验

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -22,20 +22,57 @@ const Register: FC = () => {
         </Space>
       </div>
       <div>
-        <Form labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} onFinish={onFinish}>
-          <Form.Item label="用户名" name="username">
+        <Form
+          labelCol={{ span: 7 }}
+          wrapperCol={{ span: 15 }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: "请输入用户名" },
+              {
+                type: "string",
+                min: 5,
+                max: 20,
+                message: "字符长度在5-20之间",
+              },
+              { pattern: /^\w+$/, message: "只能是字母数字下划线" },
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="密码" name="password">
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[{ required: true, message: "请输入密码" }]}
+          >
             <Input.Password />
           </Form.Item>
-          <Form.Item label="确认密码" name="confirm">
+          <Form.Item
+            label="确认密码"
+            name="confirm"
+            dependencies={["password"]} // 依赖于 password ，password 变化会重新触发 validator 验证
+            rules={[
+              { required: true, message: "请输入密码" },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  } else {
+                    return Promise.reject(new Error("两次密码不一致"));
+                  }
+                },
+              }),
+            ]}
+          >
             <Input.Password />
           </Form.Item>
           <Form.Item label="昵称" name="nickname">
             <Input />
           </Form.Item>
-          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+          <Form.Item wrapperCol={{ offset: 7, span: 15 }}>
             <Space>
               <Button type="primary" htmlType="submit">
                 注册
