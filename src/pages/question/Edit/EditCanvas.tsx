462e594bcbf73474bf8bd92f2bf4db250713daba
Vixcity <2091283625@qq.com>
2023-10-13 09:07:24 +0800

    .

--- src/pages/question/Edit/EditCanvas.tsx
+++ src/pages/question/Edit/EditCanvas.tsx
@@ -1,19 +1,32 @@
 import React, { FC } from "react";
 import { Spin } from "antd";
 import useGetComponentInfo from "../../../hooks/useGetComponentInfo";
+import { getComponentConfByType } from "../../../components/QuestionComponents";
+import { ComponentInfoType } from "../../../store/componentReducer";
 import styles from "./EditCanvas.module.scss";
 
 // 临时展示一下title和input效果
-import QuestionTitle from "../../../components/QuestionComponents/QuestionTitle/Components";
-import QuestionInput from "../../../components/QuestionComponents/QuestionInput/Components";
+// import QuestionTitle from "../../../components/QuestionComponents/QuestionTitle/Components";
+// import QuestionInput from "../../../components/QuestionComponents/QuestionInput/Components";
 
 type PropsType = {
   loading: boolean;
 };
 
+function genComponent(componentInfo: ComponentInfoType) {
+  const { type, props } = componentInfo; // 每个组件的信息，是从 redux store 中获取的
+
+  const componentConf = getComponentConfByType(type);
+
+  if (!componentConf) return;
+
+  const { Component } = componentConf;
+  return <Component {...props} />;
+}
+
 const EditCanvas: FC<PropsType> = ({ loading }) => {
   const { componentList } = useGetComponentInfo();
-
+  getComponentConfByType;
   if (loading) {
     return (
       <div style={{ textAlign: "center" }}>
@@ -24,7 +37,16 @@ const EditCanvas: FC<PropsType> = ({ loading }) => {
 
   return (
     <div className={styles.canvas}>
-      <div>
+      {componentList.map((c) => {
+        const { fe_id } = c;
+
+        return (
+          <div key={fe_id} className={styles["component-wrapper"]}>
+            <div className={styles.component}>{genComponent(c)}</div>
+          </div>
+        );
+      })}
+      {/* <div>
         <div className={styles["component-wrapper"]}>
           <div className={styles.component}>
             <QuestionTitle />
@@ -35,7 +57,7 @@ const EditCanvas: FC<PropsType> = ({ loading }) => {
             <QuestionInput />
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
